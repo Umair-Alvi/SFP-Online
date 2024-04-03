1877c2a2e7053b54422b008c7c4a25a6515b06fa
Muhammad Umair Akram <165748503+Umair-Alvi@users.noreply.github.com>
2024-04-03 09:02:28 +0500

    again Link tag Add

--- src/component/card/Card.jsx
+++ src/component/card/Card.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-// import { a , BrowserRouter} from 'react-router-dom';
+import { Link , BrowserRouter} from 'react-router-dom';
 
 // import { FaChartBar } from 'react-icons/fa'; // Importing the desired React icon
 
 
 const Card = ({Label,Icon}) => {
   return (
-    // <BrowserRouter>
+    <BrowserRouter>
     <div className="container">
-      <a href="#" style={{ textDecoration: 'none' }}>
+      <Link to="#" style={{ textDecoration: 'none' }}>
         <div className="card appCard bg-body-light border-0 shadow app p-1 mb-1 bg-body-light rounded-4">
           <div className="card-body">
             <br />
@@ -18,12 +18,12 @@ const Card = ({Label,Icon}) => {
               {Icon} {/* Using the React icon component */}
             </h5>
             <br />
-            <p className="card-text text-center"><a className="appLabel" href="#" style={{ textDecoration: 'none', color: 'black' }}>{Label}</a></p>
+            <p className="card-text text-center"><Link className="appLabel" to="#" style={{ textDecoration: 'none', color: 'black' }}>{Label}</Link></p>
           </div>
         </div>
-      </a>
+      </Link>
     </div>
-    // </BrowserRouter>
+    </BrowserRouter>
   );
 }
 
